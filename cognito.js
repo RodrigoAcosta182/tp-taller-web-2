1c486e0b82bd2fe3c75457a44d210ed199dd0b07
julioRodrigoAcosta <jracosta1991@gmail.com>
2022-06-20 21:56:46 -0300

    cognito post para login y registro

--- cognito.js
+++ cognito.js
@@ -1,64 +1,75 @@
-const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-global.fetch = require('node-fetch');
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+global.fetch = require("node-fetch");
 var express = require("express");
-
 var app = express();
+app.use(express.json())
+const port = 3000;
 
-const poolData = {    
-    UserPoolId : "us-east-1_HyWEjsPir", // Your user pool id here    
-    ClientId : "2l0oeerk50h653v43iofjtglnk" // Your client id here
-    }; 
-const pool_region = 'us-east-2';
+const poolData = {
+  UserPoolId: "us-east-1_HyWEjsPir", // Your user pool id here
+  ClientId: "2l0oeerk50h653v43iofjtglnk", // Your client id here
+};
 
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-// app.post("/registrarUsuario",(req,res) => {
-//      RegisterUser();
-// })
-
-function RegisterUser(email,password){
-   var attributeList = [];
-   attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:"Lgante Keloke"}));
-   attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"address",Value:"Enrique Segoviano"}));
-   userPool.signUp("garlopaficticia421@gmail.com", "123456789", attributeList, null, 
-   function(err, result){
-       if (err) {
-           console.log(err);
-           return;
-       }
-       cognitoUser = result.user;
-       console.log('user name is ' + cognitoUser.getUsername());
-   });
-}
-
-
- function Login() {
+app.post("/login-usuario", (req, res) => {
     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-        Username : 'jracosta1992@gmail.com',
-        Password : '123456789',
-    });
-
-    var userData = {
-        Username : 'jracosta1992@gmail.com',
-        Pool : userPool
-    };
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.authenticateUser(authenticationDetails, {
+        Username: req.body.email,
+        Password: req.body.password,
+      });
+    
+      var userData = {
+        Username: req.body.email,
+        Pool: userPool,
+      };
+      var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+      cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: function (result) {
-            console.log('access token + ' + result.getAccessToken().getJwtToken());
-            console.log('id token + ' + result.getIdToken().getJwtToken());
-            console.log('refresh token + ' + result.getRefreshToken().getToken());
+          console.log("access token + " + result.getAccessToken().getJwtToken());
+          console.log("id token + " + result.getIdToken().getJwtToken());
+          console.log("refresh token + " + result.getRefreshToken().getToken());
+          res.status(200).jsonp(req.body);
         },
-        onFailure: function(err) {
-            console.log(err);
+        onFailure: function (err) {
+          console.log(err);
+          res.status(500).send(err.message);
         },
+      });
+})
 
-    });
-}
-
-app.listen(3000, () => {
-    console.log("Servidor en -> http://localhost:3000");
+app.post("/registrar-usuario", (req, res) => {
+  var attributeList = [];
+  attributeList.push(
+    new AmazonCognitoIdentity.CognitoUserAttribute({
+      Name: "name",
+      Value: req.body.nombre,
+    })
+  );
+  attributeList.push(
+    new AmazonCognitoIdentity.CognitoUserAttribute({
+      Name: "address",
+      Value: req.body.direccion,
+    })
+  );
+  userPool.signUp(
+    req.body.email,
+    req.body.password,
+    attributeList,
+    null,
+    function (error, result) {
+      if (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+        return;
+      }
+      cognitoUser = result.user;
+      res.status(200).jsonp(req.body);
+      console.log("user name is " + cognitoUser.getUsername());
+    }
+  );
 });
 
-// RegisterUser();
-//Login();
+app.listen(port, () => {
+  console.log("Servidor en -> http://localhost puerto: " + port);
+});
+
