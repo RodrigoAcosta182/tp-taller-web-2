090b87d8c4d1a4a522f2635d16bb68c4359e2a90
julioRodrigoAcosta <jracosta1991@gmail.com>
2022-07-02 03:40:35 -0300

    finalizar compra

--- tp-taller-web-2-frontend/src/app/components/checkpedido/checkpedido.component.ts
+++ tp-taller-web-2-frontend/src/app/components/checkpedido/checkpedido.component.ts
@@ -1,29 +1,58 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { ApiService } from 'src/app/services/api/api.service';
 import { carritoProductos } from '../carrito/carritoProductos';
 
 @Component({
   selector: 'app-checkpedido',
   templateUrl: './checkpedido.component.html',
-  styleUrls: ['./checkpedido.component.css']
+  styleUrls: ['./checkpedido.component.css'],
 })
 export class CheckpedidoComponent implements OnInit, DoCheck {
-  total: number = 0
+  total: number = 0;
   carrito = carritoProductos;
-  constructor() { }
+  success!: Boolean;
+  successStr!: String;
+
+  constructor(private apiService: ApiService, private router: Router, private cookieService : CookieService) {}
 
   ngOnInit(): void {
     //reset las sesiones cuando entro al carrito
-    this.carrito.forEach(producto =>{
-      producto.cantidadSesiones = 1
-    })
+    this.carrito.forEach((producto) => {
+      producto.cantidadSesiones = 1;
+    });
   }
 
   ngDoCheck(): void {
     //calcula el total
-    this.total = 0
-    this.carrito.forEach(producto =>{
-      this.total += producto.precio * producto.cantidadSesiones
-    })
+    this.total = 0;
+    this.carrito.forEach((producto) => {
+      this.total += producto.precio * producto.cantidadSesiones;
+    });
   }
 
+  finalizarCompra(carrito: any) {
+    const dtoCompra = {
+      usuario: this.cookieService.get("token_access"),
+      productos: carrito,
+      total: this.total,
+    };
+    this.apiService.post('/finalizar-compra-producto', dtoCompra).subscribe(
+      (respuesta) => {
+        if (respuesta !== null && respuesta !== undefined) {
+          this.success = true;
+          this.successStr = respuesta as string;
+          setTimeout(() => {
+            this.success = false;
+            this.successStr = '';
+          }, 5000);
+        }
+      },
+      (err) => {
+        500;
+        this.success = false;
+      }
+    );
+  }
 }
